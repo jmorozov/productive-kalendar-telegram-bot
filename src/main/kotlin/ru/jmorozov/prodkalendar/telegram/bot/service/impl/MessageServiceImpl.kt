46ea69f3d8f0155f347e94ed9336cbee6750f372
jmorozov <mevgeny7@gmail.com>
2018-09-08 10:34:56 +0300

    Добавить поддержку предпраздничных дней и типов дней

--- src/main/kotlin/ru/jmorozov/prodkalendar/telegram/bot/service/impl/MessageServiceImpl.kt
+++ src/main/kotlin/ru/jmorozov/prodkalendar/telegram/bot/service/impl/MessageServiceImpl.kt
@@ -1,7 +1,5 @@
 package ru.jmorozov.prodkalendar.telegram.bot.service.impl
 
-import java.time.format.DateTimeParseException
-import javax.validation.ValidationException
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import org.springframework.web.client.HttpClientErrorException
@@ -10,7 +8,11 @@ import ru.jmorozov.prodkalendar.telegram.bot.service.ProductiveKalendarClient
 import ru.jmorozov.prodkalendar.telegram.bot.utils.parseDate
 import ru.jmorozov.prodkalendar.telegram.bot.utils.parseEndDate
 import ru.jmorozov.prodkalendar.telegram.bot.utils.parseStartDate
+import ru.jmorozov.prodkalendar.telegram.dto.BotResponse
 import ru.jmorozov.prodkalendar.telegram.dto.DateRange
+import ru.jmorozov.prodkalendar.telegram.dto.DayType
+import java.time.format.DateTimeParseException
+import javax.validation.ValidationException
 
 @Service
 class MessageServiceImpl @Autowired constructor(
@@ -18,52 +20,80 @@ class MessageServiceImpl @Autowired constructor(
 ) : MessageService {
 
     companion object {
-        const val VALIDATION_ERROR_MESSAGE = "Некорректные входные данные. Начало или конец интервала дат должен быть указан в формате гггг-ММ-дд"
-        val HELP = """Выходных с гггг-ММ-дд до ггг-ММ-дд - узнать количество выходных в интервале дат
-                        |Рабочих с гггг-ММ-дд до ггг-ММ-дд - узнать количество рабочих в интервале дат
-                        |гггг-ММ-дд - узнать является ли день выходным или рабочим
-                    """.trimMargin()
-        const val INCORRECT_DATE = "Некорректная дата. Дата должна соотеветствовать формату гггг-ММ-дд - 2018-08-30"
-        const val HOLIDAY = "Выходной"
-        const val WORKDAY = "Рабочий"
-        const val REST_EXCEPTION = "Упс! Произошла ошибка в сервисе производственного календаря. Попробуйте повторить позже"
-
+        val IS_HOLIDAY_REGEX = """(?<date>\d{4}-\d{2}-\d{2})\s+(выходной|ВЫХОДНОЙ)\s*\??""".toRegex()
+        val IS_HOLIDAY_TOMORROW = """[Зз][Аа]втра\s+выходной\s*\??""".toRegex()
         val DATE_REGEX = """(?<date>\d{4}-\d{2}-\d{2})""".toRegex()
     }
 
     @Suppress("ReturnCount", "ComplexMethod")
-    override fun getResponseMessage(message: String): String =
-            try {
-                when {
-                    message.startsWith("выходных", true) -> getCountBetween(message, productiveKalendarClient::getHolidaysBetween)
-                    message.startsWith("рабочих", true) -> getCountBetween(message, productiveKalendarClient::getWorkdaysBetween)
-                    DATE_REGEX.matches(message) -> getDateType(message)
-                    else -> HELP
-                }
-            } catch (dtpe: DateTimeParseException) {
-                INCORRECT_DATE
-            } catch (rce: HttpClientErrorException) {
-                if (rce.statusCode.is4xxClientError) {
-                    VALIDATION_ERROR_MESSAGE
-                } else {
-                    REST_EXCEPTION
-                }
-            }
-
-    private fun getCountBetween(message: String, parseFunc: (DateRange) -> String?): String {
-        val range = DateRange(parseStartDate(message), parseEndDate(message))
+    override fun getResponseMessage(message: String): String {
+        val trimmedMessage = message.trim()
         return try {
-            parseFunc(range) ?: VALIDATION_ERROR_MESSAGE
-        } catch (e: ValidationException) {
-            VALIDATION_ERROR_MESSAGE
+            when {
+                trimmedMessage.startsWith("выходных", true) ->
+                    getCountBetween(trimmedMessage, productiveKalendarClient::getHolidaysBetween)
+                trimmedMessage.startsWith("рабочих", true) ->
+                    getCountBetween(trimmedMessage, productiveKalendarClient::getWorkdaysBetween)
+
+                IS_HOLIDAY_REGEX.matches(trimmedMessage) -> isDateHoliday(trimmedMessage).text
+                IS_HOLIDAY_TOMORROW.matches(trimmedMessage) -> isTomorrowHoliday().text
+
+                DATE_REGEX.matches(trimmedMessage) -> getDayType(trimmedMessage).text
+                "Завтра".equals(trimmedMessage, true) -> getTomorrowType().text
+                else -> BotResponse.HELP.text
+            }
+        } catch (dtpe: DateTimeParseException) {
+            BotResponse.INCORRECT_DATE.text
+        } catch (rce: HttpClientErrorException) {
+            if (rce.statusCode.is4xxClientError) {
+                BotResponse.VALIDATION_ERROR_MESSAGE.text
+            } else {
+                BotResponse.REST_EXCEPTION.text
+            }
         }
     }
 
-    private fun getDateType(message: String): String {
-        val date = parseDate(message, DATE_REGEX, "date") ?: return INCORRECT_DATE
+    private fun getCountBetween(message: String, getCountByRange: (DateRange) -> String?): String {
+        val range = DateRange(parseStartDate(message), parseEndDate(message))
+        return try {
+            getCountByRange(range) ?: BotResponse.VALIDATION_ERROR_MESSAGE.text
+        } catch (e: ValidationException) {
+            BotResponse.VALIDATION_ERROR_MESSAGE.text
+        }
+    }
 
-        val isHoliday: Boolean = productiveKalendarClient.isHoliday(date) ?: return INCORRECT_DATE
+    private fun isDateHoliday(message: String): BotResponse {
+        val date = parseDate(message, IS_HOLIDAY_REGEX, "date") ?: return BotResponse.INCORRECT_DATE
 
-        return if (isHoliday) HOLIDAY else WORKDAY
+        val isHoliday: Boolean = productiveKalendarClient.isHoliday(date) ?: return BotResponse.INCORRECT_DATE
+
+        return if (isHoliday) BotResponse.YES else BotResponse.NO
+    }
+
+    private fun isTomorrowHoliday(): BotResponse {
+        val isHoliday: Boolean = productiveKalendarClient.isHolidayTomorrow() ?: return BotResponse.REST_EXCEPTION
+
+        return if (isHoliday) BotResponse.YES else BotResponse.NO
+    }
+
+    private fun getDayType(message: String): BotResponse {
+        val date = parseDate(message, DATE_REGEX, "date") ?: return BotResponse.INCORRECT_DATE
+
+        val dayType: DayType = productiveKalendarClient.getDayType(date) ?: return BotResponse.INCORRECT_DATE
+
+        return toBotResponse(dayType)
+    }
+
+    private fun toBotResponse(dayType: DayType): BotResponse =
+            when(dayType) {
+                DayType.PREHOLIDAY -> BotResponse.PREHOLIDAY
+                DayType.HOLIDAY -> BotResponse.HOLIDAY
+                DayType.WORKDAY -> BotResponse.WORKDAY
+            }
+
+    private fun getTomorrowType(): BotResponse {
+        val dayType: DayType = productiveKalendarClient.getTomorrowType() ?: return BotResponse.REST_EXCEPTION
+
+        return toBotResponse(dayType)
     }
 }
--- src/test/kotlin/ru/jmorozov/prodkalendar/telegram/bot/service/impl/MessageServiceImplSpec.kt
+++ src/test/kotlin/ru/jmorozov/prodkalendar/telegram/bot/service/impl/MessageServiceImplSpec.kt
@@ -6,19 +6,17 @@ import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
 import org.jetbrains.spek.api.dsl.on
 import ru.jmorozov.prodkalendar.telegram.bot.service.ProductiveKalendarClient
-import ru.jmorozov.prodkalendar.telegram.bot.service.impl.MessageServiceImpl.Companion.HELP
-import ru.jmorozov.prodkalendar.telegram.bot.service.impl.MessageServiceImpl.Companion.HOLIDAY
-import ru.jmorozov.prodkalendar.telegram.bot.service.impl.MessageServiceImpl.Companion.INCORRECT_DATE
-import ru.jmorozov.prodkalendar.telegram.bot.service.impl.MessageServiceImpl.Companion.VALIDATION_ERROR_MESSAGE
-import ru.jmorozov.prodkalendar.telegram.bot.service.impl.MessageServiceImpl.Companion.WORKDAY
+import ru.jmorozov.prodkalendar.telegram.dto.BotResponse
 import ru.jmorozov.prodkalendar.telegram.dto.DateRange
+import ru.jmorozov.prodkalendar.telegram.dto.DayType
 import java.time.LocalDate
 import java.time.Month
 
 class MessageServiceImplSpec : Spek ({
     given("Message service") {
-        val startDate = LocalDate.of(2018, Month.JANUARY, 1)
-        val endDate = LocalDate.of(2018, Month.FEBRUARY, 2)
+        val startDate = LocalDate.of(2018, Month.JANUARY, 2)
+        val endDate = LocalDate.of(2018, Month.FEBRUARY, 3)
+        val preholiday = startDate.minusDays(1)
         val holidays = "5"
         val workdays = "3"
         val productiveKalendarClient = mock(ProductiveKalendarClient::class)
@@ -30,22 +28,33 @@ class MessageServiceImplSpec : Spek ({
         ) itReturns workdays
         When calling productiveKalendarClient.isHoliday(startDate) itReturns true
         When calling productiveKalendarClient.isHoliday(endDate) itReturns false
+        When calling productiveKalendarClient.isHolidayTomorrow() itReturns true
+
+        When calling productiveKalendarClient.getDayType(startDate) itReturns DayType.HOLIDAY
+        When calling productiveKalendarClient.getDayType(preholiday) itReturns DayType.PREHOLIDAY
+        When calling productiveKalendarClient.getDayType(endDate) itReturns DayType.WORKDAY
+        When calling productiveKalendarClient.getTomorrowType() itReturns DayType.HOLIDAY
 
         val messageService = MessageServiceImpl(productiveKalendarClient)
 
         listOf(
-            Test("вЫхОднЫх с 2018-01-01 до 2018-02-02", "should return holidays count string", holidays),
-            Test("вЫхОднЫх с 2018-01-01", "should return holidays count string", holidays),
-            Test("рАбОчИх с 2018-01-01 до 2018-02-02", "should return workdays count string", workdays),
-            Test("рАбОчИх до 2022-02-02", "should return workdays count string", workdays),
-            Test("вЫхОднЫх c тогда до сейчас", "should return validation error message", VALIDATION_ERROR_MESSAGE),
-            Test("рАбОчИх c тогда до сейчас", "should return validation error message", VALIDATION_ERROR_MESSAGE),
-            Test("рАбОчИх c 2018-02-30 до 2018-02-31", "should return incorrect date message", INCORRECT_DATE),
-            Test("вЫхОднЫх с 4444-44-44", "should return incorrect date message", INCORRECT_DATE),
-            Test("2018-01-01", "should return holiday", HOLIDAY),
-            Test("2018-02-02", "should return workday", WORKDAY),
-            Test("4444-44-44", "should return incorrect date message", INCORRECT_DATE),
-            Test("Абдырда", "should return help message", HELP)
+            Test("вЫхОднЫх с 2018-01-02 до 2018-02-03", "should return holidays count string", holidays),
+            Test("вЫхОднЫх с 2018-01-02", "should return holidays count string", holidays),
+            Test(" рАбОчИх с 2018-01-02 до 2018-02-03 ", "should return workdays count string", workdays),
+            Test(" рАбОчИх до 2022-02-02 ", "should return workdays count string", workdays),
+            Test("вЫхОднЫх c тогда до сейчас", "should return validation error message", BotResponse.VALIDATION_ERROR_MESSAGE.text),
+            Test("рАбОчИх c тогда до сейчас", "should return validation error message", BotResponse.VALIDATION_ERROR_MESSAGE.text),
+            Test("рАбОчИх c 2018-02-30 до 2018-02-31", "should return incorrect date message", BotResponse.INCORRECT_DATE.text),
+            Test("вЫхОднЫх с 4444-44-44", "should return incorrect date message", BotResponse.INCORRECT_DATE.text),
+            Test("2018-01-01", "should return preholiday", BotResponse.PREHOLIDAY.text),
+            Test("2018-01-02", "should return holiday", BotResponse.HOLIDAY.text),
+            Test(" 2018-02-03 ", "should return workday", BotResponse.WORKDAY.text),
+            Test(" ЗАвТрА ", "should return holiday", BotResponse.HOLIDAY.text),
+            Test("4444-44-44", "should return incorrect date message", BotResponse.INCORRECT_DATE.text),
+            Test(" 2018-01-02 выходной ?", "should return yes", BotResponse.YES.text),
+            Test("  2018-02-03  выходной  ", "should return no", BotResponse.NO.text),
+            Test("  ЗАвтра  выходной ? ", "should return no", BotResponse.YES.text),
+            Test("Абдырда", "should return help message", BotResponse.HELP.text)
         ).forEach {
             test ->
             on("request message `${test.message}`") {
